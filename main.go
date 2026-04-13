66900b451e5a1c3c74ba1f5a3fe68212d7a0f864
kuche1 <znamsiparolata4esno@abv.bg>
2026-04-13 19:20:12 +0300

    update

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/AlecAivazis/survey/v2"
+)
 
 func main() {
 	fmt.Printf("hi\n")
+
+	var choice int
+
+	prompt := &survey.Select{
+		Message: "Choose an option:",
+		Options: []string{"Start", "Settings", "Quit"},
+	}
+
+	err := survey.AskOne(prompt, &choice)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("choice=%v\n", choice)
 }
